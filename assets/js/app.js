e7652447bbcf51340c9daf55f85dd3143536d31a
Abdullayev05 <abdullayevmahammadaga@gmail.com>
2024-01-09 10:14:49 +0400

    table

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,21 +14,56 @@
 // });
 
 
+// const input = document.querySelector("#search");
+// const btn = document.getElementById("#elave_et")
+// const add_btn = document.getElementById(".add")
+// const name_inp = document.getElementById(".name")
+// btn.addEventListener("click", () => {
+//   innerHTML += "<tr>" + "<td>" + inp.value + "</td>" + "</tr>"
+//   innerHTML += "<tr>" + "<td>" + inp.value + "</td>" + "</tr>"
+//   innerHTML += "<tr>" + "<td>" + inp.value + "</td>" + "</tr>"
+// });
+// const trs = [...document.querySelectorAll("tr")].slice(1);
+// console.log(trs);
+// input.addEventListener("change", () => {
+//   if (!input.value.trim()) {
+//     trs.map((tr) => {
+//       tr.classList.remove("green");
+//     });
+//     return;
+//   }
+//   trs.map((tr) => {
+//     tr.classList.remove("green");
+//     if (!tr.innerHTML.toLowerCase().includes(input.value.toLowerCase().trim())) {
+//       tr.classList.add("green");
+//     }
+//   });
+// });
+
 const input = document.querySelector("#search");
+const btn = document.getElementById("elave_et");
+const add_btn = document.querySelector(".add");
+const name_inp = document.querySelector(".name");
+
+btn.addEventListener("click", () => {
+  const table = document.querySelector("table");
+  table.innerHTML += "<tr>" + "<td>" + name_inp.value + "</td>" + "</tr>";
+});
+
 const trs = [...document.querySelectorAll("tr")].slice(1);
 console.log(trs);
 
-input.addEventListener("change", () => {
-    if (!input.value.trim()) {
-        trs.map((tr) => {
-            tr.classList.remove("green");
-        });
-        return;
-    }
+input.addEventListener("input", () => {
+  if (!input.value.trim()) {
     trs.map((tr) => {
-        tr.classList.remove("green");
-        if (!tr.innerHTML.toLowerCase().includes(input.value.toLowerCase().trim())) {
-            tr.classList.add("green");
-        }
+      tr.classList.remove("green");
     });
+    return;
+  }
+  trs.map((tr) => {
+    tr.classList.remove("green");
+    if (!tr.innerHTML.toLowerCase().includes(input.value.toLowerCase().trim())) {
+      tr.classList.add("green");
+    }
+  });
 });
