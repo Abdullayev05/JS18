561cf27700d7bad9cadac4e4546ec6d13eb55543
Abdullayev05 <abdullayevmahammadaga@gmail.com>
2024-01-09 10:18:24 +0400

    table

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,8 +14,8 @@
 // });
 
 
-// const input = document.querySelector("#search");
-// const btn = document.getElementById("#elave_et")
+const input = document.querySelector("#search");
+const btn = document.getElementById("#elave_et")
 // const add_btn = document.getElementById(".add")
 // const name_inp = document.getElementById(".name")
 // btn.addEventListener("click", () => {
@@ -23,37 +23,9 @@
 //   innerHTML += "<tr>" + "<td>" + inp.value + "</td>" + "</tr>"
 //   innerHTML += "<tr>" + "<td>" + inp.value + "</td>" + "</tr>"
 // });
-// const trs = [...document.querySelectorAll("tr")].slice(1);
-// console.log(trs);
-// input.addEventListener("change", () => {
-//   if (!input.value.trim()) {
-//     trs.map((tr) => {
-//       tr.classList.remove("green");
-//     });
-//     return;
-//   }
-//   trs.map((tr) => {
-//     tr.classList.remove("green");
-//     if (!tr.innerHTML.toLowerCase().includes(input.value.toLowerCase().trim())) {
-//       tr.classList.add("green");
-//     }
-//   });
-// });
-
-const input = document.querySelector("#search");
-const btn = document.getElementById("elave_et");
-const add_btn = document.querySelector(".add");
-const name_inp = document.querySelector(".name");
-
-btn.addEventListener("click", () => {
-  const table = document.querySelector("table");
-  table.innerHTML += "<tr>" + "<td>" + name_inp.value + "</td>" + "</tr>";
-});
-
 const trs = [...document.querySelectorAll("tr")].slice(1);
 console.log(trs);
-
-input.addEventListener("input", () => {
+input.addEventListener("change", () => {
   if (!input.value.trim()) {
     trs.map((tr) => {
       tr.classList.remove("green");
@@ -67,3 +39,4 @@ input.addEventListener("input", () => {
     }
   });
 });
+
